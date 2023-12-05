1e5f70a3be7bf6cd4899a2c11fbc0a85b9ea9106
sunerbao <sunchenggao.lbf@gmail.com>
2023-12-05 10:43:55 +0900

    add mattere.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GameEngine } from 'react-game-engine';
+import Matter from 'matter-js';
 import carImage from './lianxi/resource/pictures/car.png';
 
 const App = () => {
   const [positions, setPositions] = useState([{ x: 50, y: 50 }]);
-  const [currentPosition, setCurrentPosition] = useState({ x: 50, y: 50 });
+  const [carPosition, setCarPosition] = useState({ x: 50, y: 50 });
   const [carIndex, setCarIndex] = useState(0);
   const [mouseUpFlg, setMouseUpFlg] = useState(false);
   const [rotation, setRotation] = useState(0);
+
+  useEffect(() => {
+    // Create Matter.js engine
+    const engine = Matter.Engine.create();
+    const world = engine.world;
+
+    // Create a car body
+    const carBody = Matter.Bodies.rectangle(carPosition.x, carPosition.y, 60, 60, {
+      friction: 0.8,
+      restitution: 0.1,
+    });
+
+    // Add the car body to the world
+    Matter.World.add(world, [carBody]);
+
+    // Update Matter.js engine in each frame
+    const updatePhysics = () => {
+      Matter.Engine.update(engine);
+    };
+
+    // Start the engine
+    const intervalId = setInterval(updatePhysics, 1000 / 60);
+
+    // Cleanup on component unmount
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [carPosition]);
 
   const Car = ({ position, rotation }) => (
     <img
@@ -49,13 +78,13 @@ const App = () => {
 
     if (nextCarPosition && mouseUpFlg) {
       // 更新小车位置
-      setCurrentPosition({ x: nextCarPosition.x, y: nextCarPosition.y - 45 });
+      setCarPosition({ x: nextCarPosition.x, y: nextCarPosition.y - 45 });
 
       // 计算小车的旋转角度
       if (compareIndex < positions.length) {
         const comparePosition = positions[compareIndex];
-        const deltaX = comparePosition.x - currentPosition.x;
-        const deltaY = comparePosition.y - currentPosition.y;
+        const deltaX = comparePosition.x - carPosition.x;
+        const deltaY = comparePosition.y - carPosition.y;
         const rotation = Math.atan2(deltaY, deltaX) * (180 / Math.PI) - 90;
         setRotation(rotation);
       }
@@ -65,7 +94,7 @@ const App = () => {
 
     return {
       Car: {
-        position: currentPosition,
+        position: carPosition,
         rotation: rotation,
         renderer: <Car />,
       },
@@ -97,7 +126,7 @@ const App = () => {
         systems={[systemRunPath]}
         entities={{
           Car: {
-            position: currentPosition,
+            position: carPosition,
             renderer: <Car />
           },
           Path: { positions, renderer: <Path/> },
